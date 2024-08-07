be4926dbe043d21d9ad076cbd3e76a37abc8b7fb
TakoyMP4 <138342171+TakoyMP4@users.noreply.github.com>
2024-08-07 10:20:32 -0300

    alteraçoes sketch.js

--- sketch.js
+++ sketch.js
@@ -5,11 +5,6 @@
   let posicaoHorizontal;
   let posicaoVertical;
 
-  let posicaoHorizontal2;
-  let posicaoVertical2;
-
-
-
 function setup() {
   createCanvas(400, 400);
 }
@@ -19,24 +14,29 @@ function setup() {
   createCanvas(500, 500)
   cor = color(random(0, 255), random(0, 255),random (0, 255));
   background(color(100, 0 , 0));
-    posicaoHorizontal = 0;
-    posicaoVertical = random(height);
-    
-    posicaoHorizontal2 = 0;
-    posicaoVertical2 = random(height);
+    posicaoHorizontal = 400;
+    posicaoVertical = 300;
 }
     
 function draw() {
   
   fill(cor);
   circle(posicaoHorizontal, posicaoVertical, 50);
-  circle(posicaoHorizontal2, posicaoVertical2, 50);
   
-  posicaoHorizontal+= random(0,3);
-  posicaoVertical+= random(-3,5);
+  if (mouseX < posicaoHorizontal) {
+    posicaoHorizontal--;
+  }
   
-  posicaoHorizontal2+= random(0,3);
-  posicaoVertical2+= random(-3,3);
+  if (mouseX > posicaoHorizontal) {
+    posicaoHorizontal++;
+  }
+  if (mouseY < posicaoHorizontal) {
+    posicaoVertical--;
+  }
+  
+  if (mouseY > posicaoHorizontal) {
+    posicaoVertical++;
+  }
   
   if (mouseIsPressed){
     cor = color(random(0, 255), random(0, 255),random (0, 255), random(0, 100));
